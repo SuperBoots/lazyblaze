a667b341855699dc160995494c6c4110c30aa4e0
Dan Partington <34525290+SuperBoots@users.noreply.github.com>
2025-02-14 16:24:25 -0800

    Install script is doing a lot of validation and creating a new user config if it doesn't exist

---
+++ Install.ps1
@@ -0,0 +1,176 @@
+param (
+  $workingDirectory
+)
+  
+$globalPrimaryScriptName = "InstallApps"
+$globalRequireAdmin = "True"
+
+  
+##########################  Fix Working Directory  ################################
+# If launching as admin from a .bat file it will default the working directory to system32
+# so we need to pass what the working directory should be as a parameter
+$currentWorkingDirectory = (Get-Item .).FullName
+if ($null -ne $workingDirectory -and (-not($workingDirectory -like $currentWorkingDirectory))) {
+  Write-Host -ForegroundColor Yellow "Setting working directory to '$workingDirectory'"
+  Set-Location $workingDirectory
+}
+
+
+##########################  Install Steps  ################################
+# Determine install version (from InstallConfig.xml)
+# Determine install location (from InstallConfig.xml)
+#   Also check environment variable?
+# Determine installed version
+#   Does it exist?
+#   Is it the same? minor change? major change?
+# Tell user where LazyBlaze will be installed
+#   Prompt something like "press y to continue"
+# Warn user if continuing will result in a breaking change
+#   Prompt something like "press y to continue"
+# Copy sample config into install directory if it doesn't exist
+# Copy scripts from repo to install location
+#   Insert version info at the top of the scripts
+
+
+##########################  Get Install Config  ################################
+$configLocationXmlFileName = ".\InstallConfig.xml"
+if (-not (test-path -PathType leaf $configLocationXmlFileName)) {
+  Write-Host -ForegroundColor Red "Failed to find config $($configLocationXmlFileName), exiting."
+  Pause
+  Exit
+}
+$installConfig = [xml](Get-Content $configLocationXmlFileName)
+
+
+##########################  Start Logging  ################################
+$date = (Get-Date).ToString("yyyy-MM-dd_HHmmss")
+$logFile = ".\Logs\$($date)_$($globalPrimaryScriptName)_log.txt"
+Start-Transcript -Path $logFile
+$logStarted = "True"
+
+
+##########################  Determine Install Location  ################################
+$installLocation = $installConfig.settings.installdirectory.fulldirectory
+if ($installConfig.settings.installdirectory.options.useuserdirectory -like "True") {
+  $installLocation = "$($env:USERPROFILE)\$($installConfig.settings.installdirectory.userdirectory)"
+}
+Write-Host -ForegroundColor Yellow "LazyBlaze will be installed to '$($installLocation)'"
+$userInput = Read-Host "Enter 'y' to continue installation"
+if (-not ($userInput -ieq 'y')) {
+  Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
+  Pause
+  Exit
+}
+
+
+##########################  Validate Install Version  ################################
+$installMajorVersion = $installConfig.settings.version.major
+$installMinorVersion = $installConfig.settings.version.minor
+$installMinorBlocking = $installConfig.settings.version.options.minorblocking
+function IsNumeric ($Value) {
+  return $Value -match "^[\d\.]+$"
+}
+if (-not (IsNumeric $installMajorVersion)) {
+  Write-Host -ForegroundColor Red "Major version in install config not valid '$($installMajorVersion)', exiting."
+  Pause
+  Exit
+}
+if (-not (IsNumeric $installMinorVersion)) {
+  Write-Host -ForegroundColor Red "Minor version in install config not valid '$($installMinorVersion)', exiting."
+  Pause
+  Exit
+}
+
+
+##########################  Add User Config  ################################
+$userConfigFileName = "$($installLocation)Config.xml"
+if (-not (test-path -PathType leaf $userConfigFileName)) {
+  Write-Host -ForegroundColor Yellow "Existing config not found, creating '$($userConfigFileName)'"
+  if (!(Test-Path -LiteralPath $installLocation)) {
+    New-Item -ItemType Directory -Path $installLocation 
+  }
+  Copy-Item -Path ".\StarterConfig.xml" -Destination $userConfigFileName
+  $attempts = 0
+  $maxAttempts = 10
+  while ($attempts -lt $maxAttempts) {
+    try {
+      $attempts++
+      $regex = '\s*<version major=.* minor=.*></version>'
+      $existingstring = Select-String -Path $userConfigFileName -Pattern $regex
+      if (-not ($null -ne $existingstring)) {
+        Write-Host -ForegroundColor Red "Failed to find an entry for version in starter config. Exiting..."
+        Pause
+        Exit
+      }
+      $newVersionLine = "  <version major=""$($installMajorVersion)"" minor=""$($installMinorVersion)""></version>"
+      (Get-Content $userConfigFileName) -replace $regex, $newVersionLine | Set-Content $userConfigFileName
+      Write-Host -ForegroundColor Green "Set user config version info to $($newVersionLine)"
+      break
+    } catch [System.Exception] {
+      $_
+      if ($attempts -gt ($maxAttempts - 1)) {
+        Write-Host -ForegroundColor Red "Set Config Version has reached max allowable attempts (of $($maxAttempts)), abandoning ship."
+        Pause
+        Exit
+      }
+      Write-Host -ForegroundColor Red "Set Config Version attempt $($attempts) failed, sleeping 1 second before attempting again."
+      Start-Sleep -Seconds 1
+    }
+  }
+}
+
+
+##########################  Check User Config Version  ################################
+$userConfig = [xml](Get-Content $userConfigFileName)
+$userConfigMajorVersion = $userConfig.settings.version.major
+$userConfigMinorVersion = $userConfig.settings.version.minor
+if (-not (IsNumeric $userConfigMajorVersion)) {
+  Write-Host -ForegroundColor Red "Major version '$($userConfigMajorVersion)' in user config $($userConfigFileName) not valid, exiting."
+  Pause
+  Exit
+}
+if (-not (IsNumeric $userConfigMinorVersion)) {
+  Write-Host -ForegroundColor Red "Minor version '$($userConfigMinorVersion)' in user config $($userConfigFileName) not valid, exiting."
+  Pause
+  Exit
+}
+if (($userConfigMajorVersion -gt $installMajorVersion) -or (($userConfigMajorVersion -eq $installMajorVersion) -and ($userConfigMinorVersion -gt $installMinorVersion))) {
+  # User has somehow ended up with a user config version that's more recent than the installer that's running.
+  Write-Host -ForegroundColor Red "You've somehow got a version of your user config that's more recent than the installer. I'm going to be honest, I'm confused. I give up."
+  Pause
+  Exit
+}
+if ($userConfigMajorVersion -lt $installMajorVersion) {
+  # User has a config that is old enough that the install has breaking changes
+  Write-Host -ForegroundColor Red "ERROR: The installer has a more recent major version ($($installMajorVersion)) than the user config $($userConfigFileName) ($($userConfigMajorVersion
+)). Updated major versions indicate breaking changes, please bring your user config up to date before trying again."
+  Pause
+  Exit
+}
+if (($userConfigMajorVersion -eq $installMajorVersion) -and ($userConfigMinorVersion -lt $installMinorVersion)) {
+  # User has a config that is slightly out of date, default behavior is to block installation but this can be overridden by the minorblocking setting in the install config.
+  Write-Host -ForegroundColor Yellow "Warning: The install config InstallConfig.xml has a more recent minor version ($($installMinorVersion
+)) than the user config $($userConfigFileName) ($($userConfigMinorVersion)). Updated minor versions indicate non-breaking changes but you may want to review the example config."
+  if ($null -eq $installMinorBlocking -or $installMinorBlocking -like "True") {
+    Write-Host -ForegroundColor Red "Exiting script. If you want to allow this script to continue with minor version differences set the minorblocking property to False in the install config InstallConfig.xml"
+    Pause
+    Exit
+  }
+}
+
+
+##########################  Copy Scripts To Install Location  ################################
+
+
+##########################  Copy Other Files To Install Location  ################################
+
+
+##########################  Stop Logging  ################################
+if ($logStarted -like "True") {
+  Stop-Transcript
+}
+
+
+##########################  Success Message  ################################
+Write-Host -ForegroundColor Green "Execution of script $($globalPrimaryScriptName) successfully finished."
+Pause
