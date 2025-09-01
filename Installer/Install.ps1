7b615495b370f014b20a91ba154cbcd3be0882a9
Dan Partington <34525290+SuperBoots@users.noreply.github.com>
2025-09-01 00:07:25 -0700

    Update install process to be more intuitive (#58)

--- Installer/Install.ps1
+++ Installer/Install.ps1
@@ -6,6 +6,13 @@ $globalPrimaryScriptName = "Install"
 $globalRequireAdmin = "True"
 
 
+##########################  Start Logging  ################################
+$date = (Get-Date).ToString("yyyy-MM-dd_HHmmss")
+$logFile = ".\Logs\$($date)_$($globalPrimaryScriptName)_log.txt"
+Start-Transcript -Path $logFile
+$logStarted = "True"
+
+
 ##########################  Fix Working Directory  ################################
 # If launching as admin from a .bat file it will default the working directory to system32
 # so we need to pass what the working directory should be as a parameter
@@ -53,21 +60,15 @@ if (-not (test-path -PathType leaf $configLocationXmlFileName)) {
 $installConfig = [xml](Get-Content $configLocationXmlFileName)
 
 
-##########################  Start Logging  ################################
-$date = (Get-Date).ToString("yyyy-MM-dd_HHmmss")
-$logFile = ".\Logs\$($date)_$($globalPrimaryScriptName)_log.txt"
-Start-Transcript -Path $logFile
-$logStarted = "True"
-
-
 ##########################  Determine Install Location  ################################
 $installOverrideEnvVarName = "LZB_INSTALL_OVERRIDE"
 $configDirectoryOverride = $installConfig.settings.installdirectory.directoryoverride
 $envDirectoryOverride = [Environment]::GetEnvironmentVariable($installOverrideEnvVarName, 'User')
 if (-not ($null -eq $configDirectoryOverride -or $configDirectoryOverride -like "")) {
-  Write-Host -ForegroundColor Yellow "The directoryoverride setting in InstallConfig.xml has been set and will be used for install location."
-  Write-Host -ForegroundColor Yellow "This will install LazyBlaze to the override location, and also save the location to the $($installOverrideEnvVarName) environment variable for future use."
-  Write-Host -ForegroundColor Yellow "LazyBlaze will be installed to '$($configDirectoryOverride)'"
+  # Use directoryoverride setting from InstallConfig.xml for install location
+  Write-Host "The directoryoverride setting in InstallConfig.xml has been set and will be used for install location."
+  Write-Host "This will install LazyBlaze to the override location, and also save the location to the $($installOverrideEnvVarName) environment variable for future use."
+  Write-Host -ForegroundColor Yellow "LazyBlaze installation location is set to '$($configDirectoryOverride)'"
   $userInput = Read-Host "Enter 'y' to continue installation"
   if (-not ($userInput -ieq 'y')) {
     Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
@@ -79,8 +80,9 @@ if (-not ($null -eq $configDirectoryOverride -or $configDirectoryOverride -like 
   $installLocation = $configDirectoryOverride
 }
 elseif (-not ($null -eq $envDirectoryOverride -or $envDirectoryOverride -like "")) {
+  # Use saved environment variable for install location
   Write-Host -ForegroundColor Yellow "The user environment variable $($installOverrideEnvVarName) was found to have a value and will be used for install location."
-  Write-Host -ForegroundColor Yellow "LazyBlaze will be installed to '$($envDirectoryOverride)'"
+  Write-Host -ForegroundColor Yellow "LazyBlaze installation location is set to '$($envDirectoryOverride)'"
   $userInput = Read-Host "Enter 'y' to continue installation"
   if (-not ($userInput -ieq 'y')) {
     Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
@@ -90,10 +92,12 @@ elseif (-not ($null -eq $envDirectoryOverride -or $envDirectoryOverride -like ""
   $installLocation = $envDirectoryOverride
 }
 else {
+  # Use default combination of user name and machine name for install location in OneDrive
   $sysMachineName = Invoke-Expression -Command 'hostname'
-  Write-Host -ForegroundColor Yellow "Building default install location using user directory $($env:USERPROFILE) and machine name $($sysMachineName)."
+  Write-Host "Building default install location using user directory $($env:USERPROFILE) and machine name $($sysMachineName)."
   $defaultInstallLocation = "$($env:USERPROFILE)\OneDrive\LazyBlaze_$($sysMachineName)\"
-  Write-Host -ForegroundColor Yellow "LazyBlaze will be installed to '$($defaultInstallLocation)'"
+  Write-Host -ForegroundColor Yellow "LazyBlaze installation location is set to '$($defaultInstallLocation)'"
+  Write-Host "If you would like to change the installation location you can choose to not continue this installation, set the directoryoverride value in InstallConfig.xml, and re-run this installer."
   $userInput = Read-Host "Enter 'y' to continue installation"
   if (-not ($userInput -ieq 'y')) {
     Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
@@ -107,7 +111,6 @@ else {
 ##########################  Validate Install Version  ################################
 $installMajorVersion = $installConfig.settings.version.major
 $installMinorVersion = $installConfig.settings.version.minor
-$installMinorBlocking = $installConfig.settings.version.options.minorblocking
 function IsNumeric ($Value) {
   return $Value -match "^[\d\.]+$"
 }
@@ -123,41 +126,30 @@ if (-not (IsNumeric $installMinorVersion)) {
 }
 
 
+##########################  Check For Existing Install  ################################
+if (Test-Path -LiteralPath $installLocation) {
+  Write-Host -ForegroundColor Yellow "An existing lazyblaze install was found."
+  Write-Host "Continuing with this installation will overwrite existing scripts, but user specific configuration will not be affected." 
+  $userInput = Read-Host "Enter 'y' to update your LazyBlaze"
+  if (-not ($userInput -ieq 'y')) {
+    Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
+    Pause
+    Exit
+  }
+}
+
+
 ##########################  Add User Config  ################################
 $userConfigFileName = "$($installLocation)Config.xml"
+$configVersionRegex = '\s*<version major=.* minor=.*></version>'
+$newVersionLineForConfig = "  <version major=""$($installMajorVersion)"" minor=""$($installMinorVersion)""></version>"
 if (-not (test-path -PathType leaf $userConfigFileName)) {
-  Write-Host -ForegroundColor Yellow "Existing config not found, creating '$($userConfigFileName)'"
+  Write-Host "Existing config not found, creating '$($userConfigFileName)'"
   if (!(Test-Path -LiteralPath $installLocation)) {
     New-Item -ItemType Directory -Path $installLocation 
   }
   Copy-Item -Path ".\FilesToInstall\Config.xml" -Destination $userConfigFileName
-  $attempts = 0
-  $maxAttempts = 10
-  while ($attempts -lt $maxAttempts) {
-    try {
-      $attempts++
-      $regex = '\s*<version major=.* minor=.*></version>'
-      $existingstring = Select-String -Path $userConfigFileName -Pattern $regex
-      if (-not ($null -ne $existingstring)) {
-        Write-Host -ForegroundColor Red "Failed to find an entry for version in starter config. Exiting..."
-        Pause
-        Exit
-      }
-      $newVersionLine = "  <version major=""$($installMajorVersion)"" minor=""$($installMinorVersion)""></version>"
-      (Get-Content $userConfigFileName) -replace $regex, $newVersionLine | Set-Content $userConfigFileName
-      Write-Host -ForegroundColor Green "Set user config version info to $($newVersionLine)"
-      break
-    } catch [System.Exception] {
-      $_
-      if ($attempts -gt ($maxAttempts - 1)) {
-        Write-Host -ForegroundColor Red "Set Config Version has reached max allowable attempts (of $($maxAttempts)), abandoning ship."
-        Pause
-        Exit
-      }
-      Write-Host -ForegroundColor Red "Set Config Version attempt $($attempts) failed, sleeping 1 second before attempting again."
-      Start-Sleep -Seconds 1
-    }
-  }
+  ReplaceLine -LineRegex $configVersionRegex -NewLine $newVersionLineForConfig -File $userConfigFileName
 }
 
 
@@ -183,20 +175,28 @@ if (($userConfigMajorVersion -gt $installMajorVersion) -or (($userConfigMajorVer
 }
 if ($userConfigMajorVersion -lt $installMajorVersion) {
   # User has a config that is old enough that the install has breaking changes
-  Write-Host -ForegroundColor Red "ERROR: The installer has a more recent major version ($($installMajorVersion)) than the user config $($userConfigFileName) ($($userConfigMajorVersion
-)). Updated major versions indicate breaking changes, please bring your user config up to date before trying again."
-  Pause
-  Exit
-}
-if (($userConfigMajorVersion -eq $installMajorVersion) -and ($userConfigMinorVersion -lt $installMinorVersion)) {
-  # User has a config that is slightly out of date, default behavior is to block installation but this can be overridden by the minorblocking setting in the install config.
-  Write-Host -ForegroundColor Yellow "Warning: The install config InstallConfig.xml has a more recent minor version ($($installMinorVersion
-)) than the user config $($userConfigFileName) ($($userConfigMinorVersion)). Updated minor versions indicate non-breaking changes but you may want to review the example config."
-  if ($null -eq $installMinorBlocking -or $installMinorBlocking -like "True") {
-    Write-Host -ForegroundColor Red "Exiting script. If you want to allow this script to continue with minor version differences set the minorblocking property to False in the install config InstallConfig.xml"
+  Write-Host "The installer has a more recent major version ($($installMajorVersion)) than the user config $($userConfigFileName) ($($userConfigMajorVersion
+)). Updated major versions indicate breaking changes. please bring your user config up to date before trying again."
+  Write-Host -ForegroundColor Red "IF YOU CHOOSE TO CONTINUE, YOUR INSTALLATION IS HIGHLY LIKELY TO BE IN A BROKEN STATE UNTIL YOU UPDATE YOUR USER CONFIG STRUCTURE (AND POSSIBLY OTHER ITEMS). YOU HAVE BEEN WARNED."
+  $userInput = Read-Host "Enter 'y' to update your LazyBlaze from $($userConfigMajorVersion).$($userConfigMinorVersion) to $($installMajorVersion).$($installMinorVersion)"
+  if (-not ($userInput -ieq 'y')) {
+    Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
     Pause
     Exit
   }
+  ReplaceLine -LineRegex $configVersionRegex -NewLine $newVersionLineForConfig -File $userConfigFileName
+}
+if (($userConfigMajorVersion -eq $installMajorVersion) -and ($userConfigMinorVersion -lt $installMinorVersion)) {
+  # User has a config that is slightly out of date, changes are either not breaking or minimally breaking.
+  Write-Host "The install config InstallConfig.xml has a more recent minor version ($($installMinorVersion
+)) than the user config $($userConfigFileName) ($($userConfigMinorVersion)). Updated minor versions indicate non-breaking changes but you may want to review the example config for small changes."
+  $userInput = Read-Host "Enter 'y' to update your LazyBlaze from $($userConfigMajorVersion).$($userConfigMinorVersion) to $($installMajorVersion).$($installMinorVersion)"
+  if (-not ($userInput -ieq 'y')) {
+    Write-Host -ForegroundColor Red "Input did not match 'y', exiting."
+    Pause
+    Exit
+  }
+  ReplaceLine -LineRegex $configVersionRegex -NewLine $newVersionLineForConfig -File $userConfigFileName
 }
 
 
